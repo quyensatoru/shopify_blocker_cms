9340c19df046cdf657766399a1a81123316ee2a3
Quyen PV <quyenpv@bsscommerce.com>
2025-07-21 11:16:27 +0700

    build base remix shopify app

---
+++ app/shopify.server.ts
@@ -0,0 +1,60 @@
+import "@shopify/shopify-app-remix/adapters/node";
+import {
+  ApiVersion,
+  AppDistribution,
+  shopifyApp,
+} from "@shopify/shopify-app-remix/server";
+import {MemorySessionStorage} from "@shopify/shopify-app-session-storage-memory"
+import {SQLiteSessionStorage} from "@shopify/shopify-app-session-storage-sqlite"
+import webhooks from "./webhooks"
+import * as process from "node:process";
+
+const cache = `${process.cwd()}/database.sqlite`;
+
+const shopify = shopifyApp({
+  apiKey: process.env.SHOPIFY_API_KEY,
+  apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
+  apiVersion: ApiVersion.January25,
+  scopes: process.env.SCOPES?.split(","),
+  appUrl: process.env.SHOPIFY_APP_URL || "",
+  authPathPrefix: "/auth",
+  sessionStorage: process.env.NODE_ENV === "development" ? new MemorySessionStorage() : new SQLiteSessionStorage(cache) ,
+  distribution: AppDistribution.AppStore,
+  future: {
+    unstable_newEmbeddedAuthStrategy: true,
+    removeRest: true,
+  },
+  hooks: {
+    afterAuth: async ({session}) => {
+      await shopify.registerWebhooks({session: session});
+      try {
+        await fetch(`${process.env.API_URL}/shop`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            ...session
+          })
+        })
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  },
+  webhooks: {
+    ...webhooks,
+  },
+  ...(process.env.SHOP_CUSTOM_DOMAIN
+    ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
+    : {}),
+});
+
+export default shopify;
+export const apiVersion = ApiVersion.January25;
+export const addDocumentResponseHeaders = shopify.addDocumentResponseHeaders;
+export const authenticate = shopify.authenticate;
+export const unauthenticated = shopify.unauthenticated;
+export const login = shopify.login;
+export const registerWebhooks = shopify.registerWebhooks;
+export const sessionStorage = shopify.sessionStorage;
